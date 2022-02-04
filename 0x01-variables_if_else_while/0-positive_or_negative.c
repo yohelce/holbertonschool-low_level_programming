aed5cabf8c7ee62e0627658b028f4e1aa275bfd1
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-04 12:39:48 -0800

    Agregué 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - program will assign a random number to the variable n... 
- *Return: Always 0 (Success)
+/**
+ * main - program will assign a random number to the variable n...
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
