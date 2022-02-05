940f7548fc35a1f926d895914fce2d4a75066eee
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-04 17:11:46 -0800

    Agregué 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * starting from 0, followed by a new line with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
