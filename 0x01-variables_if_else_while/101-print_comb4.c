0016e4244727c511258382e5e6dc3475efe54823
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-04 22:20:26 -0800

    Agregué 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			for (l = 50; l <= 57; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
