30318026d486e8ae5b99d731252ddcef98a8f7f0
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-08 21:45:30 -0800

    Agregué 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a: first integer input
+ * @b: second integer input
+ *
+ * Return: result of addition
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
