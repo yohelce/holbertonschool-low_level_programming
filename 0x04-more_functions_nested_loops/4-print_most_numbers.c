8668c273b0a698173e79d20671fad076091e89fc
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-10 13:02:25 -0800

    Agregué 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,12 @@ void print_most_numbers(void)
 	int n;
 
 	n = 48;
-	while (n < 58 && (n != 50 || n != 52))
+	while (n < 58)
 	{
-		_putchar(n);
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
 		n++;
 	}
 	_putchar(10);
