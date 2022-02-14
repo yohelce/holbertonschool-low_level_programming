c30009cd5e9c541defa914b59a1c55cb0841b6b7
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-14 13:47:46 -0800

    Agregué 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - akes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * @n: parameter or pointer to n
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
