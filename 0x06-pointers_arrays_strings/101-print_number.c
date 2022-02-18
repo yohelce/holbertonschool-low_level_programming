1e398552961c84eaad9d0e4767d37efddcd3f534
Your Name <yohel.cruz.espinoza@gmail.com>
2022-02-18 13:22:39 -0800

    Agregué 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
