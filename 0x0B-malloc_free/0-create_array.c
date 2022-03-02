43ec4ebd0e2e56931f105855fd52d9ce66dd4243
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-01 16:44:17 -0800

    Agregué 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	if (p == '\0')
-		return (0);
-
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+                return (0);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	p[i] = '\0';
