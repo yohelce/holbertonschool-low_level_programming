b5d9d3050b1cc99b9b550cf3ff0173708fa7dfc9
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-03 18:25:54 -0800

    Modifiqué 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc(sizeof(int) * (max - min +1));
+	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++)
