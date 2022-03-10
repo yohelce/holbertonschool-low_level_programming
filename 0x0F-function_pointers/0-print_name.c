309eab4b675b728c071d9b11e53d523c94497531
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-09 19:16:33 -0800

    Agregué 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls another function to print name
+ * @name: pointer to string name to print
+ * @f: pointer to function that prints
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
