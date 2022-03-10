9febaf086f78a9ae0990bc7068b5799281fc3c53
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-09 20:37:02 -0800

    Agregué 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: input array
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: integer found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	return (-1);
+}
