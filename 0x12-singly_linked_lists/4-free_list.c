bd56e037494a785288caa38c0d4424bb245c0b32
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-17 21:58:14 -0700

    Added function free a list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - frees linked list of all allocated memory
+ * @head: beginning of linked list
+ *
+ * Return: void, only frees memory
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
