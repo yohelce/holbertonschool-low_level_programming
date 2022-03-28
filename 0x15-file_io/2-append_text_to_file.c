21914190d1ae5aa070fb1367c3f17031979e31dc
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-28 13:14:55 -0700

    Added function 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[str])
 			str++;
-		wr = write(fd, text_content, str)
+		wr = write(fd, text_content, str);
 		if (wr == -1)
 		{
 			close(fd);
