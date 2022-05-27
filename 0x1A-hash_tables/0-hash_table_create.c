7dc43dad9caf01c9a868d6482c294e3bacfaec15
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-27 13:35:11 -0700

    Added 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hashtable empty
+ * @size: size of the array in the hashtable
+ * 
+ * Return: new hashtable or null if fails
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL || size == 0) 
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	return (ht);
+}
