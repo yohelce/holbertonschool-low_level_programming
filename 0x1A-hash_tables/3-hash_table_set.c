4328c271ae8246c0424be5ad8cfd4dfe7039b5d0
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-27 21:18:02 -0700

    Added 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table to add the element to
+ * @key: key of the element, will give the index in the array
+ * @value: value associated with the key
+ *
+ * Return: 1 if is SUCCESS or 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *temp;
+
+	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+
+	hash_node = malloc(sizeof(hash_node_t));
+	if (hash_node == NULL)
+		return (0);
+
+	hash_node->key = strdup(key);
+	hash_node->value = strdup(value);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (!strcmp(temp->key, key))
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+}
