b6e208b42eaef17c435f5eaf30da2e937ae6a5b3
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-27 21:59:32 -0700

    Added 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i, temp = 0;
+
+	if (ht != NULL)
+	{
+		printf("{");
+
+		i = 0;
+		while (i < ht->size)
+		{
+			if (ht->array[i] != NULL)
+			{
+				if (temp == 0)
+				{
+					printf("'%s': '%s'", ht->array[i]->key,
+					       ht->array[i]->value);
+					temp = 1;
+				}
+				else
+					printf(", '%s': '%s'", ht->array[i]->key,
+					       ht->array[i]->value);
+				
+				node = ht->array[i]->next;
+				while (node != NULL)
+				{
+					printf("'%s': '%s'", node->key, node->value);
+					node = node->next;
+				}
+			}
+			i++;
+		}
+		printf("}\n");
+	}
+}
