50501244a64b4512d4bfb79348f8d84c201794bf
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-29 18:56:17 -0700

    Fixed 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *hash_node = NULL, *temp = NULL;
 
-	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
+	if (!ht || !key || !*key || !value || !ht->array || strlen(key) == 0)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
@@ -41,9 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	hash_node = malloc(sizeof(hash_node_t));
-	if (hash_node == NULL)
-		return (0);
+
 	hash_node->key = strdup(key);
 	hash_node->value = strdup(value);
 	hash_node->next = hash_node;
