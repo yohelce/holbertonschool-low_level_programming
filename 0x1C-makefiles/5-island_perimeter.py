c5ad59c4a32832627e002adff3532e2ec1e8e19e
Your Name <yohel.cruz.espinoza@gmail.com>
2022-07-14 23:30:52 -0700

    Added 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,6 +22,6 @@ def island_perimeter(grid):
                 sizes += 4
                 if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                if (grid[i - 1][j] == 1):
+                if (i > 0  and grid[i - 1][j] == 1):
                     edges += 1
     return sizes - (edges * 2)
