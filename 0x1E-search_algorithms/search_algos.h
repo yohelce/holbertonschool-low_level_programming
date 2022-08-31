eb59f88d1a15031cd33bab903208ee5edf7500ed
Your Name <yohel.cruz.espinoza@gmail.com>
2022-08-31 11:54:59 -0700

    Fixed parameter index

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,15 +15,15 @@
 typedef struct listint_s
 {
 	int n;
-	size_t index;
+	size_t idx;
 	struct listint_s *next;
 } listint_t;
 
 /** Prototypes **/
-int			linear_search(int *array, size_t size, int value);
-int			binary_search(int *array, size_t size, int value);
-int			jump_search(int *array, size_t size, int value);
-int			interpolation_search(int *array, size_t size, int value);
-int			exponential_search(int *array, size_t size, int value);
-int			advanced_binary(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif /*SEARCH_ALGOS_H*/
